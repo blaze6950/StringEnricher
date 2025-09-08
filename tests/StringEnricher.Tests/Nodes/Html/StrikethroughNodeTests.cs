0ddd14fd43d08521f3190bab6158085d091f1d5e
Mykyta Zotov <mykyta.zotov@ihsmarkit.com>
2025-09-08 14:41:00 +0200

    Refactor test namespaces for HTML and MarkdownV2 nodes to improve organization

---
+++ tests/StringEnricher.Tests/Nodes/Html/StrikethroughNodeTests.cs
@@ -0,0 +1,53 @@
+using StringEnricher.Helpers.Html;
+
+namespace StringEnricher.Tests.Nodes.Html;
+
+public class StrikethroughNodeTests
+{
+    [Fact]
+    public void Test()
+    {
+        // Arrange
+        const string expectedStrikethrough = "<s>strikethrough</s>";
+
+        // Act
+        var styledStrikethrough = StrikethroughHtml.Apply("strikethrough").ToString();
+
+        // Assert
+        Assert.NotNull(styledStrikethrough);
+        Assert.NotEmpty(styledStrikethrough);
+        Assert.Equal(expectedStrikethrough, styledStrikethrough);
+    }
+
+    [Fact]
+    public void TryGetChar_ValidIndices_ReturnsTrueAndCorrectChar()
+    {
+        // Arrange
+        var strikethrough = StrikethroughHtml.Apply("test");
+        const string expected = "<s>test</s>";
+
+        // Act & Assert
+        for (var i = 0; i < expected.Length; i++)
+        {
+            var result = strikethrough.TryGetChar(i, out var ch);
+            Assert.True(result);
+            Assert.Equal(expected[i], ch);
+        }
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(11)] // "<s>test</s>" length is 11
+    public void TryGetChar_OutOfRangeIndices_ReturnsFalseAndNullChar(int index)
+    {
+        // Arrange
+        var strikethrough = StrikethroughHtml.Apply("test");
+
+        // Act
+        var result = strikethrough.TryGetChar(index, out var ch);
+
+        // Assert
+        Assert.False(result);
+        Assert.Equal('\0', ch);
+    }
+}
---
+++ tests/StringEnricher.Tests/Nodes/MarkdownV2/BlockquoteNodeTests.cs
@@ -0,0 +1,63 @@
+﻿using StringEnricher.Helpers.MarkdownV2;
+
+namespace StringEnricher.Tests.Nodes.MarkdownV2;
+
+public class BlockquoteNodeTests
+{
+    [Fact]
+    public void Test()
+    {
+        // Arrange
+        const string expectedCodeBlock =
+            ">Block quotation started\n>Block quotation continued\n>The last line of the block quotation";
+
+        // Act
+        var styledCodeBlock = BlockquoteMarkdownV2
+            .Apply("Block quotation started\nBlock quotation continued\nThe last line of the block quotation")
+            .ToString();
+
+        // Assert
+        Assert.NotNull(styledCodeBlock);
+        Assert.NotEmpty(styledCodeBlock);
+        Assert.Equal(expectedCodeBlock, styledCodeBlock);
+    }
+
+    [Fact]
+    public void Test_TryGetChar()
+    {
+        // Arrange
+        var blockQuote =
+            BlockquoteMarkdownV2.Apply(
+                "Block quotation started\nBlock quotation continued\nThe last line of the block quotation");
+        const string expectedCodeBlock =
+            ">Block quotation started\n>Block quotation continued\n>The last line of the block quotation";
+        var expectedTotalLength = expectedCodeBlock.Length;
+
+        // Act & Assert
+        for (var i = 0; i < expectedTotalLength; i++)
+        {
+            var result = blockQuote.TryGetChar(i, out var actualChar);
+
+            Assert.True(result);
+            Assert.Equal(expectedCodeBlock[i], actualChar);
+        }
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(89)] // expected string length is 88
+    public void Test_TryGetChar_OutOfRange_ShouldReturnFalse(int index)
+    {
+        // Arrange
+        var blockQuote =
+            BlockquoteMarkdownV2.Apply(
+                "Block quotation started\nBlock quotation continued\nThe last line of the block quotation");
+
+        // Act
+        var result = blockQuote.TryGetChar(index, out var actualChar);
+
+        // Assert
+        Assert.False(result);
+        Assert.Equal('\0', actualChar);
+    }
+}
