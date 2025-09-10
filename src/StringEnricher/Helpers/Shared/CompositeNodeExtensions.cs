e1d80d5310fda7ad8607c0df0fddc62437844939
Mykyta Zotov <mykyta.zotov@ihsmarkit.com>
2025-09-10 23:31:15 +0200

    Add overloads to CompositeNodeExtensions for combining primitive types with nodes

--- src/StringEnricher/Helpers/Shared/CompositeNodeExtensions.cs
+++ src/StringEnricher/Helpers/Shared/CompositeNodeExtensions.cs
@@ -30,6 +30,8 @@ public static class CompositeNodeExtensions
         where TLeft : INode
         where TRight : INode => new(left, right);
 
+    #region Left Node with Primitive Right Node
+
     /// <summary>
     /// Combines two nodes into a single composite node.
     /// </summary>
@@ -37,13 +39,13 @@ public static class CompositeNodeExtensions
     /// The left node.
     /// </param>
     /// <param name="right">
-    /// The right node.
+    /// The right string.
     /// </param>
     /// <typeparam name="TLeft">
     /// The type of the left node.
     /// </typeparam>
     /// <returns>
-    /// A new composite node that combines the left and right nodes.
+    /// A new composite node that combines the left node and the string.
     /// </returns>
     public static CompositeNode<TLeft, PlainTextNode> CombineWith<TLeft>(this TLeft left, string right)
         where TLeft : INode => new(left, right);
@@ -376,4 +378,357 @@ public static class CompositeNodeExtensions
         string? format = null,
         IFormatProvider? provider = null)
         where TLeft : INode => new(left, right.ToNode(format, provider));
+
+    #endregion
+
+    #region Primitive Left Node with Right Node
+
+    /// <summary>
+    /// Combines two nodes into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The left string.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the string and right nodes.
+    /// </returns>
+    public static CompositeNode<PlainTextNode, TRight> CombineWith<TRight>(this string left, TRight right)
+        where TRight : INode => new(left, right);
+
+    /// <summary>
+    /// Combines a node with a character into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The left character.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the character and the right node.
+    /// </returns>
+    public static CompositeNode<CharNode, TRight> CombineWith<TRight>(this char left, TRight right)
+        where TRight : INode => new(left, right);
+
+    /// <summary>
+    /// Combines a node with a boolean value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The boolean value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the boolean value and the right node.
+    /// </returns>
+    public static CompositeNode<BoolNode, TRight> CombineWith<TRight>(this bool left, TRight right)
+        where TRight : INode => new(left, right);
+
+    /// <summary>
+    /// Combines a node with a DateOnly value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The DateOnly value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the DateOnly value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the DateOnly value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the DateOnly value and the right node.
+    /// </returns>
+    public static CompositeNode<DateOnlyNode, TRight> CombineWith<TRight>(this DateOnly left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a DateTime value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The DateTime value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the DateTime value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the DateTime value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the DateTime value and the right node.
+    /// </returns>
+    public static CompositeNode<DateTimeNode, TRight> CombineWith<TRight>(this DateTime left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a DateTimeOffset value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The DateTimeOffset value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the DateTimeOffset value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the DateTimeOffset value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the DateTimeOffset value and the right node.
+    /// </returns>
+    public static CompositeNode<DateTimeOffsetNode, TRight> CombineWith<TRight>(this DateTimeOffset left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a decimal value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The decimal value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the decimal value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the decimal value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the decimal value and the right node.
+    /// </returns>
+    public static CompositeNode<DecimalNode, TRight> CombineWith<TRight>(this decimal left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a double value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The double value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the double value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the double value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the double value and the right node.
+    /// </returns>
+    public static CompositeNode<DoubleNode, TRight> CombineWith<TRight>(this double left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a float value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The float value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the float value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the float value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the float value and the right node.
+    /// </returns>
+    public static CompositeNode<FloatNode, TRight> CombineWith<TRight>(this float left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a Guid value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The Guid value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the Guid value. If null, the default format will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the Guid value and the right node.
+    /// </returns>
+    public static CompositeNode<GuidNode, TRight> CombineWith<TRight>(this Guid left,
+        TRight right,
+        string? format = null)
+        where TRight : INode => new(left.ToNode(format), right);
+
+    /// <summary>
+    /// Combines a node with an integer value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The integer value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the integer value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the integer value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the integer value and the right node.
+    /// </returns>
+    public static CompositeNode<IntegerNode, TRight> CombineWith<TRight>(this int left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a long value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The long value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the long value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the long value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the long value and the right node.
+    /// </returns>
+    public static CompositeNode<LongNode, TRight> CombineWith<TRight>(this long left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a TimeOnly value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The TimeOnly value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the TimeOnly value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the TimeOnly value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the TimeOnly value and the right node.
+    /// </returns>
+    public static CompositeNode<TimeOnlyNode, TRight> CombineWith<TRight>(this TimeOnly left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    /// <summary>
+    /// Combines a node with a TimeSpan value into a single composite node.
+    /// </summary>
+    /// <param name="left">
+    /// The TimeSpan value.
+    /// </param>
+    /// <param name="right">
+    /// The right node.
+    /// </param>
+    /// <param name="format">
+    /// The format string to use when formatting the TimeSpan value. If null, the default format will be used.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider to use when formatting the TimeSpan value. If null, the current culture will be used.
+    /// </param>
+    /// <typeparam name="TRight">
+    /// The type of the right node.
+    /// </typeparam>
+    /// <returns>
+    /// A new composite node that combines the TimeSpan value and the right node.
+    /// </returns>
+    public static CompositeNode<TimeSpanNode, TRight> CombineWith<TRight>(this TimeSpan left,
+        TRight right,
+        string? format = null,
+        IFormatProvider? provider = null)
+        where TRight : INode => new(left.ToNode(format, provider), right);
+
+    #endregion
 }
