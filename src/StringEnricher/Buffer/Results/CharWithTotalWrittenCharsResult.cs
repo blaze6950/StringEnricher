dd1a5c61ecb5f831f1b017905c899e21d583531a
Mykyta Zotov <mykyta.zotov@ihsmarkit.com>
2025-11-16 00:11:40 +0100

    Add CharWithTotalWrittenCharsResult record to encapsulate character and length information

---
+++ src/StringEnricher/Buffer/Results/CharWithTotalWrittenCharsResult.cs
@@ -0,0 +1,6 @@
+﻿namespace StringEnricher.Buffer.Results;
+
+/// <summary>
+/// Represents a result containing a character and its total length.
+/// </summary>
+public record struct CharWithTotalWrittenCharsResult(bool Success, char Char, int CharsWritten);
