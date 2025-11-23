9c108bf5e4d80a3948124d221e21d61b916a9e32
Mykyta Zotov <mykyta.zotov@ihsmarkit.com>
2025-11-23 17:59:22 +0100

    Add methods to get total and escaped character counts for ISpanFormattable values

--- src/StringEnricher/Extensions/SpanFormattableExtensions.cs
+++ src/StringEnricher/Extensions/SpanFormattableExtensions.cs
@@ -52,7 +52,101 @@ public static class SpanFormattableExtensions
 
         return length;
     }
-    
+
+    /// <summary>
+    /// Gets the total number of characters and the number of escaped characters
+    /// in the formatted representation of an <see cref="ISpanFormattable"/> value.
+    /// </summary>
+    /// <param name="value">
+    /// The <see cref="ISpanFormattable"/> value to format into a configurable buffer and get the character counts from.
+    /// </param>
+    /// <param name="escapePredicate">
+    /// The predicate function to determine if a character needs to be escaped.
+    /// </param>
+    /// <param name="nodeSettings">
+    /// The node settings to use for buffer allocation.
+    /// </param>
+    /// <param name="format">
+    /// The format string.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type of the <see cref="ISpanFormattable"/> value.
+    /// </typeparam>
+    /// <returns>
+    /// A <see cref="TotalAndEscapedCharCountsResult"/> containing the total number of characters
+    /// and the number of escaped characters in the formatted representation.
+    /// </returns>
+    public static TotalAndEscapedCharCountsResult GetTotalAndEscapedCharsCounts<T>(
+        this T value,
+        Func<char, bool> escapePredicate,
+        NodeSettings nodeSettings,
+        string? format = null,
+        IFormatProvider? provider = null
+    ) where T : ISpanFormattable
+    {
+        // prepare state - the value and everything needed for formatting into an allocated buffer
+        var state = new FormattingState<T>(value, format, provider);
+
+        var charCountsResult = BufferUtils
+            .AllocateBuffer<EscapeCharsCalculator<T>, FormattingState<T>, TotalAndEscapedCharCountsResult>(
+                processor: new EscapeCharsCalculator<T>(escapePredicate),
+                state: in state,
+                nodeSettings: (NodeSettingsInternal)nodeSettings
+            );
+
+        return charCountsResult;
+    }
+
+    /// <summary>
+    /// Gets the total number of characters and the number of escaped characters
+    /// in the formatted representation of an <see cref="ISpanFormattable"/> value.
+    /// </summary>
+    /// <param name="value">
+    /// The <see cref="ISpanFormattable"/> value to format into a configurable buffer and get the character counts from.
+    /// </param>
+    /// <param name="escaper">
+    /// The function that determines if a character needs to be escaped.
+    /// </param>
+    /// <param name="nodeSettings">
+    /// The node settings to use for buffer allocation.
+    /// </param>
+    /// <param name="format">
+    /// The format string.
+    /// </param>
+    /// <param name="provider">
+    /// The format provider.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type of the <see cref="ISpanFormattable"/> value.
+    /// </typeparam>
+    /// <returns>
+    /// A <see cref="CustomTotalAndEscapedCharCountsResult"/> containing the total number of characters
+    /// and the number of escaped characters in the formatted representation.
+    /// </returns>
+    public static CustomTotalAndEscapedCharCountsResult GetCustomTotalAndEscapedCharsCounts<T>(
+        this T value,
+        Func<char, string?> escaper,
+        NodeSettings nodeSettings,
+        string? format = null,
+        IFormatProvider? provider = null
+    ) where T : ISpanFormattable
+    {
+        // prepare state - the value and everything needed for formatting into an allocated buffer
+        var state = new FormattingState<T>(value, format, provider);
+
+        var charCountsResult = BufferUtils
+            .AllocateBuffer<CustomEscapeCharsCalculator<T>, FormattingState<T>, CustomTotalAndEscapedCharCountsResult>(
+                processor: new CustomEscapeCharsCalculator<T>(escaper),
+                state: in state,
+                nodeSettings: (NodeSettingsInternal)nodeSettings
+            );
+
+        return charCountsResult;
+    }
+
     /// <summary>
     /// Gets the character at the specified index from the formatted representation of an <see cref="ISpanFormattable"/> value.
     /// </summary>
@@ -94,17 +188,26 @@ public static class SpanFormattableExtensions
         var state = new IndexedState<FormattingState<T>>(
             new FormattingState<T>(value, format, provider), index);
 
-        // tries to allocate a buffer and use the processor to get the length of the formatted value
-        var result = BufferUtils.AllocateBuffer<
-            CharAtIndexProcessor<T>,
-            IndexedState<FormattingState<T>>,
-            CharWithTotalWrittenCharsResult>
-        (
-            processor: new CharAtIndexProcessor<T>(),
-            state: in state,
-            nodeSettings: (NodeSettingsInternal)nodeSettings,
-            initialBufferLengthHint: initialBufferLengthHint ?? index + 1 // at least enough to cover the requested index
-        );
+        CharWithTotalWrittenCharsResult result;
+        try
+        {
+            // tries to allocate a buffer and use the processor to get the length of the formatted value
+            result = BufferUtils.AllocateBuffer<
+                CharAtIndexProcessor<T>,
+                IndexedState<FormattingState<T>>,
+                CharWithTotalWrittenCharsResult>
+            (
+                processor: new CharAtIndexProcessor<T>(),
+                state: in state,
+                nodeSettings: (NodeSettingsInternal)nodeSettings,
+                initialBufferLengthHint: initialBufferLengthHint ??
+                                         index + 1 // at least enough to cover the requested index
+            );
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return new CharWithTotalWrittenCharsResult(false, '\0', 0);
+        }
 
         return result;
     }
